c731bd7814f00170c95077d561132dd2368705f7
Dhruv Sehgal <dhruvsehgal@Dhruvs-MacBook-Air.local>
2026-03-15 17:45:10 +0530

    add NextAuth Authentication Setup

---
+++ src/middleware.ts
@@ -0,0 +1,37 @@
+import { NextRequest, NextResponse } from "next/server";
+import { getToken } from "next-auth/jwt";
+export { default } from 'next-auth/middleware'
+
+
+export async function middleware(request: NextRequest) {
+
+    const token = await getToken({req : request})
+    const url = request.nextUrl
+
+    if (token && 
+        (
+            url.pathname.startsWith('/') ||
+            url.pathname.startsWith('/sign-up') ||
+            url.pathname.startsWith('/sign-in') ||
+            url.pathname.startsWith('/verify') 
+        ) 
+    ) {
+        return NextResponse.redirect(new URL('/dashboard' , request.url))
+    }
+
+    if (!token && url.pathname.startsWith('/dashboard')) {
+        return NextResponse.redirect(new URL('/sign-in', request.url))
+    }
+    
+    return NextResponse.next()
+}
+
+export const config = {
+    matcher : [
+        '/',
+        '/sign-up',
+        '/sign-in',
+        '/dashboard/:path*',
+        '/verify/:path*'
+    ]
+}
