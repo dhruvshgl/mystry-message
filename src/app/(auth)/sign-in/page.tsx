e8710b4f9423441a807210836e09e0e3cd768a46
Dhruv Sehgal <dhruvsehgal@Dhruvs-MacBook-Air.local>
2026-03-20 01:25:52 +0530

    Add sign-in UI

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,8 +1,111 @@
-import React from 'react'
+"use client"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import * as z from "zod"
+import { useEffect, useState } from 'react'
+import Link from "next/link"
+import { useDebounceCallback} from "usehooks-ts"
+import { toast } from "sonner"  // toast deprecated now in sonner , so read docs before using
+import { useRouter } from "next/navigation"
+import { signUpSchema } from "@/schemas/signUpSchema"
+import axios , { AxiosError } from "axios"
+import { ApiResponse } from "@/types/ApiResponse"
+import { Input } from "@/components/ui/input"
+import {
+  Form,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { Button } from "@/components/ui/button"
+import { Loader2 } from "lucide-react"
+import { signInSchema } from "@/schemas/signInSchema"
+import { signIn } from "next-auth/react"
 
-const page = () => {
+const page = () => {    
+    const router = useRouter()
+
+    // ZOD implementation
+    const form = useForm<z.infer<typeof signInSchema>>({
+      resolver: zodResolver(signInSchema),
+      defaultValues: {
+        identifier: '',
+        password: ''
+      }
+    })
+
+    
+    const onSubmit = async (data: z.infer<typeof signInSchema>) => {
+      const result = await signIn("credentials" , {
+        redirect: false,
+        identifier: data.identifier,
+        password: data.password
+      })
+
+      if (result?.error) {
+        toast.error("Login failed" , {
+          description: "Incorrect username or password",
+        })
+      }
+
+      if (result?.url) {
+        router.replace('/dashboard')
+      }
+    }
+
+
   return (
-    <div>page</div>
+    <div className="flex justify-center items-center min-h-screen bg-gray-100">
+      <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
+        <div className="text-center">
+          <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-6">
+            Join Mystery Message
+          </h1>
+          <p className="mb-4">Sign in to start your anonymous adventure</p>
+        </div>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+            
+            <FormField
+              name="identifier"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email/Username</FormLabel>
+                  <Input {...field} name="email" />
+                  <p className='text-muted text-gray-400 text-sm'>We will send you a verification code</p>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              name="password"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <Input type="password" {...field} name="password" />
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit" >
+              Sign In
+            </Button>
+          </form>
+        </Form>
+        <div className="text-center mt-4">
+          <p>
+            First time user?{' '}
+            <Link href="/sign-up" className="text-blue-600 hover:text-blue-800">
+              Sign Up
+            </Link>
+          </p>
+        </div>
+      </div>
+    </div>
   )
 }
 
---
+++ src/app/u/[username]/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const message = () => {
+  return (
+    <div>message page</div>
+  )
+}
+
+export default message
